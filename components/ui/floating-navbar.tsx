1ae59cb6c277805092d058b6198e6453bf92b8c2
sudwiptokm <sudwiptokmondal@gmail.com>
2024-08-21 01:15:05 +0600

    fix: navbar indication

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -1,81 +1,92 @@
 "use client";
-import React, { useState } from "react";
-import {
-  motion,
-  AnimatePresence,
-  useScroll,
-  useMotionValueEvent,
-} from "framer-motion";
-import Link from "next/link";
 import { cn } from "@/lib/utils";
+import {
+	AnimatePresence,
+	motion,
+	useMotionValueEvent,
+	useScroll,
+} from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
 
 export const FloatingNav = ({
-  navItems,
-  className,
+	navItems,
+	className,
 }: {
-  navItems: {
-    name: string;
-    link: string;
-    icon?: JSX.Element;
-  }[];
-  className?: string;
+	navItems: {
+		name: string;
+		link: string;
+		icon?: JSX.Element;
+	}[];
+	className?: string;
 }) => {
-  const { scrollYProgress } = useScroll();
+	const { scrollYProgress } = useScroll();
+	const pathName = usePathname();
 
-  const [visible, setVisible] = useState(false);
+	const [visible, setVisible] = useState(false);
 
-  useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
-    if (typeof current === "number") {
-      let direction = current! - scrollYProgress.getPrevious()!;
+	useMotionValueEvent(scrollYProgress, "change", (current) => {
+		// Check if current is not undefined and is a number
+		if (typeof current === "number") {
+			const direction = current! - scrollYProgress.getPrevious()!;
 
-      if (scrollYProgress.get() < 0.05) {
-        setVisible(true);
-      } else {
-        if (direction < 0) {
-          setVisible(true);
-        } else {
-          setVisible(true);
-        }
-      }
-    }
-  });
+			if (scrollYProgress.get() < 0.05) {
+				setVisible(true);
+			} else {
+				if (direction < 0) {
+					setVisible(true);
+				} else {
+					setVisible(true);
+				}
+			}
+		}
+	});
 
-  return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        initial={{
-          opacity: 1,
-          y: -100,
-        }}
-        animate={{
-          y: visible ? 0 : -100,
-          opacity: visible ? 1 : 0,
-        }}
-        transition={{
-          duration: 0.2,
-        }}
-        className={cn(
-          "flex bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-gray-100  w-fit mx-auto fixed top-10 inset-x-0 border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-5 py-2.5  items-center justify-center gap-4",
-          className
-        )}
-      >
-        <Image src={"/oonkoo_logo.svg"} height={50} width={50} alt="logo" />
-        {navItems.map((navItem: any, idx: number) => (
-          <Link
-            key={`link=${idx}`}
-            href={navItem.link}
-            className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-500 hover:text-neutral-200"
-            )}
-          >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
-          </Link>
-        ))}
-  
-      </motion.div>
-    </AnimatePresence>
-  );
+	return (
+		<AnimatePresence mode="wait">
+			<motion.div
+				initial={{
+					opacity: 1,
+					y: -100,
+				}}
+				animate={{
+					y: visible ? 0 : -100,
+					opacity: visible ? 1 : 0,
+				}}
+				transition={{
+					duration: 0.2,
+				}}
+				className={cn(
+					"flex bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border-gray-100  w-fit mx-auto fixed top-10 inset-x-0 border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-5 py-2.5  items-center justify-center gap-4",
+					className,
+				)}
+			>
+				<Image src={"/oonkoo_logo.svg"} height={50} width={50} alt="logo" />
+				{navItems.map(
+					(
+						navItem: {
+							name: string;
+							link: string;
+							icon?: JSX.Element;
+						},
+						idx: number,
+					) => (
+						<Link
+							key={`link=${navItem.name}`}
+							href={navItem.link}
+							className={cn(
+								"relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-500 hover:text-neutral-200",
+								pathName === navItem.link && "text-neutral-200 font-semibold",
+							)}
+						>
+							<span className="block sm:hidden">{navItem.icon}</span>
+							<span className="hidden sm:block text-sm">{navItem.name}</span>
+						</Link>
+					),
+				)}
+			</motion.div>
+		</AnimatePresence>
+	);
 };
