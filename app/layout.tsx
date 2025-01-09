2d1d792cd9177fa8c601e9b8e25b1d11afdf5e8b
chnspart <imchn24@gmail.com>
2025-01-09 04:06:00 -0500

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
